4a12273a563ca0f2682fd314001fde5d0af6f1b8
GageHoechst <hoechstg99@gmail.com>
2024-03-22 10:40:35 -0400

    Added a create action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,13 @@ class PetsController < ApplicationController
     @pets = Pet.all
     render :index
   end
+
+  def create
+    @pet = Pet.create(
+      name: params[:name],
+      image: params[:image],
+      price: params[:price],
+    )
+    render :show
+  end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -11,4 +11,10 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Pet.count, data.length
   end
+  test "create" do
+    assert_difference "Pet.count", 1 do
+      post "/pets.json", params: { name: "lake", image: "water", price: 600 }
+      assert_response 200
+    end
+  end
 end
